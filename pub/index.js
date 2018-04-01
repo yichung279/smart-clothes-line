f64e0d7b86a738ba6f0756469511d201fc4a9520
yichung <ql3895619@gmail.com>
2018-04-01 08:48:50 +0800

    7777777

--- pub/index.js
+++ pub/index.js
@@ -2,27 +2,45 @@ var app = new Vue({
   el:'#app',
   data: {
     state: 'reeling in',
-    cloNum:  0
+    notifyConfig : {
+        body: `Your clothes are now ${this.state}`, // 設定內容
+        //icon: '/images/favicon.ico' // 設定 icon
+    },
   },
   methods:{
     check: function(){
       that = this
       setInterval(() => {
+        preState = this.state
         $.ajax({
           method: "get",
           url: "./ajax_data",
           success: function(res) {
             that.state = res.state
             that.cloNum = res.cloNum
+            if (preState != that.state){that.notificate()}
             console.log(res)
           }
         })
       }, 1000)
-     
-      
-      
-      
+    },
+    notificate: function(){
+      that = this
+      if (!('Notification' in window)) {return}
+       console.log(Notification.permission) 
+      if (Notification.permission === 'default' || Notification.permission === 'undefined') {
+        console.log('aaa')
+        Notification.requestPermission(function(permission) {
+          if (permission === 'granted') { 
+            var notification = new Notification('Hi there!', {body: 'welcome'}) 
+          }
+        })
+      }else if (Notification.permission === 'granted') { 
+        var notification = new Notification('Hi there!', that.notifyConfig) 
+      }
     }
+      
+      
   }
 })
 
--- sever.js
+++ sever.js
@@ -1,18 +1,26 @@
 const express =require('express')
 const app = express()
+const fs = require('fs')
 const port =16666
-
+var key = fs.readFileSync('ssl/privkey.pem')
+var cert = fs.readFileSync( 'ssl/cert.pem' )
+var ca = fs.readFileSync( 'ssl/fullchain.pem' )
+var options = {
+  key: key,
+  cert: cert,
+  ca: ca
+}
 state = 'reeling in'
 cloNum = 0
 
 app.use(express.static(__dirname+'/pub'));
 
-app.get("/get",function(req, res) {
-  if(req == undefined)
+app.get("/get", (req, res) => {
+  console.log('reseive request')
+  console.log(req.query.state)
+  if(req != undefined)
     state = req.query.state
-    cloNum = req.query.cloNum
-    console.log(cloNum)
-    res.send()
+    res.send('aaa')
 });
 
 
@@ -20,6 +28,5 @@ app.get("/ajax_data",function(req, res) {
   res.send({'state':state, 'cloNum':cloNum})
 });
 
-app.listen(port,()=>{
-  console.log(`Listening on port ${port}`)
-});
+var https = require('https')
+https.createServer(options, app).listen(port)
